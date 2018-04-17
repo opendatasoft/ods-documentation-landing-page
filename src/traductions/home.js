3858f3b44a302954848a95bbca3f5116ec33ed50
Thibaut Dupré <thibaut.dupre@opendatasoft.com>
2018-04-17 17:24:36 +0200

    Updated translations for homepage

--- src/traductions/home.js
+++ src/traductions/home.js
@@ -1,34 +1,34 @@
 export default {
 
     descBottom: {
-        en: "Can't find what you're looking for? You can send a mail to <a href='mailto: support@opendatasoft.com'>support@opendatasoft.com</a>",
-        fr: "",
-        es: "",
-        de: "",
-        nl: ""
+        en: "Can't find what you're looking for? You can send a mail to ",
+        fr: "Vous ne trouvez pas ce que vous cherchez ? Vous pouvez envoyer un email à ",
+        es: "¿No encuentras lo que buscas? Puedes enviar un email a ",
+        de: "Sie finden nicht, wonach Sie suchen? Senden Sie eine E-Mail an ",
+        nl: "Kunt u niet vinden waarnaar u op zoek bent? U kunt een e-mail sturen naar "
     },
 
     metas: {
         title: {
             en: "Home",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Accueil",
+            es: "Inicio",
+            de: "Startseite",
+            nl: "Home"
         },
         description: {
-            en: "Home",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Welcome to OpenDataSoft's help hub - the centralized help site for all OpenDataSoft's products ",
+            fr: "Bienvenu au centre d'aide OpenDataSoft - le site qui centralise l'aide pour tous les produits OpenDataSoft",
+            es: "Bienvenido a la central de ayuda deOpenDataSoft, el sitio de ayuda centralizado para todos los productos deOpenDataSoft",
+            de: "Willkommen im Hilfebereich von OpenDataSoft - der zentralen Hilfeseite für alle Produkte von OpenDataSoft",
+            nl: "Welkom op de help hub van OpenDataSoft- de gecentraliseerde helpsite voor alle producten vanOpenDataSoft"
         },
         image: {
             en: "Platform",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Platform",
+            es: "Platform",
+            de: "Platform",
+            nl: "Platform"
         }
     },
 
@@ -36,24 +36,24 @@ export default {
 
         title: {
             en: "Platform",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Plateforme",
+            es: "Plataforma",
+            de: "Plattform",
+            nl: "Platform"
         },
         desc: {
-            en: "Discover what OpenDataSoft's solution can do for you, how to publish your data, design dashboards, beautiful visualisations and much more",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Discover what OpenDataSoft's solution can do for you: how to publish data, design dashboards, beautiful visualisations, and much more.",
+            fr: "Découvrez ce que la solution OpenDataSoft peut faire pour vous: comment publier des données, concevoir des tableaux de bord, de belles visualisations, et bien plus encore.",
+            es: "Descubra qué puede hacer por usted la solución deOpenDataSoft: cómo publicar datos, diseñar paneles, bonitas visualizaciones, etc.",
+            de: "Entdecken Sie, wie die Lösung von OpenDataSoft Ihnen helfen kann: erfahren Sie, wie man Daten veröffentlicht, Dashboards und tolle Visualisierungen gestaltet und vieles mehr.",
+            nl: "Ontdek wat de oplossingen van OpenDataSoftvoor u kunnen doen: hoe u data kunt publiceren, dashboards kunt ontwerpen, mooie weergaves en nog veel meer."
         },
         url: {
             en: "https://help.opendatasoft.com/platform/",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "https://help.opendatasoft.com/platform/fr/",
+            es: "https://help.opendatasoft.com/platform/es/",
+            de: "https://help.opendatasoft.com/platform/de/",
+            nl: "https://help.opendatasoft.com/platform/nl/"
         }
 
     },
@@ -62,24 +62,24 @@ export default {
 
         title: {
             en: "Discovery",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Discovery",
+            es: "Discovery",
+            de: "Discovery",
+            nl: "Discovery"
         },
         desc: {
             en: "A curated selection of demonstrations and use cases with live examples, code excerpts and tips to turn your portal into a powerhouse!",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Une sélection de démonstrations et de cas d'utilisation avec des exemples en direct, des extraits de code et des conseils pour rendre votre portail ultra-dynamique!",
+            es: "Una selección exclusiva de demostraciones y casos de uso con ejemplos en vivo, fragmentos de código y consejos para convertir su portal en un centro neurálgico de comunicación.",
+            de: "Eine hilfreiche Auswahl an Demonstrationen und Anwendungsfällen mit konkreten Beispielen, Code-Auszügen und Tipps, um das Beste aus Ihrem Portal herauszuholen!",
+            nl: "Een uitgelezen selectie van demonstraties en use cases met live voorbeelden, code excerpts en tips om uw portaal om te toveren tot een powerhouse!"
         },
         url: {
             en: "https://help.opendatasoft.com/discovery/",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "https://help.opendatasoft.com/discovery/",
+            es: "https://help.opendatasoft.com/discovery/",
+            de: "https://help.opendatasoft.com/discovery/",
+            nl: "https://help.opendatasoft.com/discovery/"
         }
 
     },
@@ -87,25 +87,25 @@ export default {
     faq: {
 
         title: {
-            en: "FAQ/Glosssary",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "FAQ/Glossary",
+            fr: "FAQ/Glossaire",
+            es: "Preguntas frecuentes/Glosario",
+            de: "FAQ/Glossar",
+            nl: "FAQ/Lexicon"
         },
         desc: {
-            en: "If you have questions about file formats, supported browsers or metadata or simply wondering what a word means",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Here you will find answers to your questions about file formats, supported browsers or metadata or simply a word meaning ",
+            fr: "Vous trouverez ici des réponses à vos questions sur les formats de fichiers, les navigateurs pris en charge ou les métadonnées ou simplement une signification de mot",
+            es: "Aquí encontrará las respuestas a sus preguntas sobre formatos de archivos, navegadores admitidos o metadatos, o también qué significa una palabra.",
+            de: "Hier finden Sie Antworten auf Ihre Fragen zu Dateiformaten, unterstützten Browsern oder Metadaten oder die Erklärung einzelner Begriffe",
+            nl: "Hier vindt u antwoorden op uw vragen over bestandsformaten, ondersteunde browsers en metadata of gewoon de betekenis van een woord"
         },
         url: {
             en: "https://help.opendatasoft.com/faq/",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "https://help.opendatasoft.com/faq/fr/",
+            es: "https://help.opendatasoft.com/faq/es/",
+            de: "https://help.opendatasoft.com/faq/de/",
+            nl: "https://help.opendatasoft.com/faq/nl/"
         }
 
     },
@@ -114,24 +114,24 @@ export default {
 
         title: {
             en: "Widgets",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Widgets",
+            es: "Widgets",
+            de: "Widgets",
+            nl: "Widgets"
         },
         desc: {
-            en: "Discover OpenDataSoft's widgets, a library of web components to bild rich and interactive pages of data visualization",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Discover OpenDataSoft's widgets library, a selection of web components to build rich and interactive pages to visualize data.",
+            fr: "Découvrez la bibliothèque de widgets OpenDataSoft, une sélection de composants Web pour créer des pages riches et interactives afin de visualiser les données.",
+            es: "Descubra la biblioteca de widgets de OpenDataSoft, una selección de componentes web para crear páginas interactivas y de calidad a fin de visualizar datos.",
+            de: "Entdecken Sie die Widgets-Bibliothek von OpenDataSoft: hier finden Sie unterschiedliche Webkomponenten, mit denen Sie ansprechende und interaktive Seiten zur Datenvisualisierung gestalten können.",
+            nl: "Ontdek de widgets-bibliotheek van OpenDataSoft, een selectie van web components om rijke en interactieve pagina's op te bouwen om data te bekijken."
         },
         url: {
             en: "https://help.opendatasoft.com/widgets/",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "https://help.opendatasoft.com/widgets/",
+            es: "https://help.opendatasoft.com/widgets/",
+            de: "https://help.opendatasoft.com/widgets/",
+            nl: "https://help.opendatasoft.com/widgets/"
         }
 
     },
@@ -140,24 +140,24 @@ export default {
 
         title: {
             en: "Widgets Tutorial",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Tutoriel Widgets",
+            es: "Tutoriales de los Widgets",
+            de: "WidgetsTutorial",
+            nl: "WidgetsTutorial"
         },
         desc: {
             en: "Step by step guided trainings to discover OpenDataSoft's widgets from integrating a map to your own interactive dashboard",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "Des formations guidées pas à pas pour découvrir les widgets d'OpenDataSoft depuis l'intégration d'une carte à votre propre tableau de bord interactif",
+            es: "Cursos guiados paso a paso para descubrir los widgets de OpenDataSoft: desde la integración de un mapa hasta sus propios paneles interactivos.",
+            de: "Detaillierte Schulungen, bei denen die Widgets von OpenDataSoft vorgestellt werden: von der Einbindung einer Karte bis zur Gestaltung Ihres eigenen interaktiven Dashboards",
+            nl: "Een stap voor stap opleiding om de widgets van OpenDataSoft te ontdekken, van het integreren van een kaart tot uw eigen interactief dashboard"
         },
         url: {
             en: "https://help.opendatasoft.com/tutorials/",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "https://help.opendatasoft.com/tutorials/",
+            es: "https://help.opendatasoft.com/tutorials/",
+            de: "https://help.opendatasoft.com/tutorials/",
+            nl: "https://help.opendatasoft.com/tutorials/"
         }
 
     },
@@ -166,17 +166,17 @@ export default {
 
         title: {
             en: "APIs",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            fr: "APIs",
+            es: "API",
+            de: "APIs",
+            nl: "APIs"
         },
         desc: {
-            en: "Our APIs references, with endpoints, parameters you can use and consoles to run interactive queries",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Our API references, with endpoints, parameters you can use and consoles to run interactive queries",
+            fr: "Nos références d'API, avec les points terminaux, les paramètres que vous pouvez utiliser et les consoles pour exécuter des requêtes interactives",
+            es: "Nuestra referencia de API, con extremos, parámetros que se pueden utilizar y consolas para ejecutar consultas interactivas",
+            de: "Unsere API-Referenzen mit Endpunkten, Parametern, die Sie verwenden können, und Konsolen zum Ausführen interaktiver Abfragen",
+            nl: "Onze API-referenties met eindpunten, parameters die u kunt gebruiken en consoles om interactieve onderzoeken te voeren"
         },
         url: {
             en: "apis",
