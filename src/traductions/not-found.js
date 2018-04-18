71b3107ebe1697309bc31b255502c6e3a676a32f
Thibaut Dupré <thibaut.dupre@opendatasoft.com>
2018-04-18 10:02:04 +0200

    Translated Page Not Found

--- src/traductions/not-found.js
+++ src/traductions/not-found.js
@@ -1,37 +1,37 @@
 export default {
 
     message404: {
-        en: 'Page not found',
-        fr: 'La page n\'a pas été trouvé',
-        es: "",
-        de: "",
-        nl: ""
+        en: "Page not found",
+        fr: "La page n'a pas été trouvée",
+        es: "No se ha encontrado la página",
+        de: "Seite nicht gefunden",
+        nl: "Pagina niet gevonden"
     },
 
     backHome: {
-        en: 'Get back to Homepage',
-        fr: 'Retour vers la page d\'accueil',
-        es: "",
-        de: "",
-        nl: ""
+        en: "Return to homepage",
+        fr: "Retour à page d'accueil",
+        es: "Volver a página principal",
+        de: "Zurück zur Startseite",
+        nl: "Terug naar de homepagina"
     },
 
     /* Metas */
 
     metas: {
         title: {
-            en: "Platform",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "Page not found",
+            fr: "La page n'a pas été trouvée",
+            es: "No se ha encontrado la página",
+            de: "Seite nicht gefunden",
+            nl: "Pagina niet gevonden"
         },
         description: {
-            en: "Platform",
-            fr: "",
-            es: "",
-            de: "",
-            nl: ""
+            en: "The page you were looking for could not be found",
+            fr: "La page que vous cherchiez n'a pas pu être trouvée",
+            es: "No se ha podido encontrar la página que estaba buscando.",
+            de: "Die von Ihnen gesuchte Seite konnte nicht gefunden werden",
+            nl: "De pagina die je zoekt, kon niet worden gevonden"
         }
     }
 
