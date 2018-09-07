7a32bcc76de17f908fd86aad7c8cb86604a60180
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-09-07 17:45:05 +0200

    [ch14572] remove branch test for deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,10 +14,3 @@ if [ "${TRAVIS_BRANCH}" = "develop" ]; then
   cd ../ods-tutorial
   rsync -avz -e "ssh -i ../.travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
 fi
-
-if [ "${TRAVIS_BRANCH}" = "feature/14572_setup_the_infrastructure_ods_tutorial" ]; then
-  cd ods-helphub
-  rsync -avz -e "ssh -i ../.travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P dist/index.html dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_HELPHUB"
-  cd ../ods-tutorial
-  rsync -avz -e "ssh -i ../.travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
-fi
