8ef3736ec4344b3817693c5235ddd28f6cab668b
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-09-07 11:59:21 +0200

    [ch14572] test travis conf for 2 projects

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,22 @@
 set -ev
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-helphub/dist/index.html ./ods-helphub/dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_PROD_PATH_HELPHUB"
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-tutorial/build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_PROD_PATH_TUTORIAL"
+  cd ods-helphub
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P dist/index.html dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_PROD_PATH_HELPHUB"
+  cd ods-tutorial
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_PROD_PATH_TUTORIAL"
 fi
 
 if [ "${TRAVIS_BRANCH}" = "develop" ]; then
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-helphub/dist/index.html ./ods-helphub/dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_HELPHUB"
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-tutorial/build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
+  cd ods-helphub
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P dist/index.html dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_HELPHUB"
+  cd ods-tutorial
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
 fi
 
 if [ "${TRAVIS_BRANCH}" = "feature/14572_setup_the_infrastructure_ods_tutorial" ]; then
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-helphub/dist/index.html ./ods-helphub/dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_HELPHUB"
-  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P ./ods-tutorial/build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
+  cd ods-helphub
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P dist/index.html dist/assets "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_HELPHUB"
+  cd ods-tutorial
+  rsync -avz -e "ssh -i .travis.deploy -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete -P build/ "$DEPLOY_USER"@"$DEPLOY_IP":"$DEPLOY_BETA_PATH_TUTORIAL"
 fi
