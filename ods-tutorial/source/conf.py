8eaeabbb42a096bce9a6f164ef1fda78406d32ab
Emmanuel Magnier <emmanuel.magnier@opendatasoft.com>
2018-09-13 21:46:08 +0200

    Fix cobfiguration to be able to push translations to the same transifex project as main documentation - move the index to tutorials.rst instead of index.rst - remove the script that cleans up the resource in transifex

--- ods-tutorial/source/conf.py
+++ ods-tutorial/source/conf.py
@@ -54,7 +54,7 @@ source_suffix = ['.rst']
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'tutorials'
 
 # General information about the project.
 now = datetime.datetime.now()
